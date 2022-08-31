8a56251c22332673762ca4070bc024925f19a505
Nikhil Gunti <nikhilbabu829@gmail.com>
2022-08-31 14:43:37 +0530

    adding text to the textView when a button is pressed

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var display : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        display = findViewById(R.id.displayText)
     }
     fun onDigit(view:View){
-        Toast.makeText(this,"clicked",Toast.LENGTH_SHORT).show()
+        display?.append((view as Button).text);
     }
 }
