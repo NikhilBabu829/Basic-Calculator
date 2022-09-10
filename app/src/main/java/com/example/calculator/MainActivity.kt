4b5a0d589cd1ef8da49ec469767a3f73e4f09584
Nikhil Gunti <nikhilbabu829@gmail.com>
2022-09-10 11:56:33 +0530

    adding the other funcitons so that we have decimal point and -ve sign restrictions

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var lastNumeric : Boolean = false;
+    var lastDecimal : Boolean = false;
     private var display : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +18,38 @@ class MainActivity : AppCompatActivity() {
     }
     fun onDigit(view:View){
         display?.append((view as Button).text);
+        lastNumeric = true;
+        lastDecimal = false;
     }
     fun onClear(view:View){
         display?.text = ""
+        lastNumeric = false;
+        lastDecimal = false;
+    }
+    fun onDecimal(view: View){
+        if(lastNumeric && !lastDecimal){
+            display?.append(".");
+            lastDecimal = true;
+            lastNumeric = false;
+        }
+    }
+
+    fun onOperator(view: View){
+        display?.text.let {
+            if(lastNumeric && !onOperatorAdded(it.toString())){
+                display?.append((view as Button).text)
+                lastNumeric = false
+                lastDecimal = false
+            }
+        }
+    }
+
+    fun onOperatorAdded(value: String):Boolean{
+        return if(value.startsWith("-")){
+            false
+        }
+        else{
+            value.contains("+")||value.contains("-")||value.contains("x")||value.contains("/")
+        }
     }
 }
